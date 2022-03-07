af777fd9509202bf2e16e29778c39b054f6533ac
Lucas Carvalho Medeiros <lucas.medeiros5@fatec.sp.gov.br>
2022-03-07 19:40:06 +0000

    salvando

---
+++ Repetição.java
@@ -0,0 +1,16 @@
+public class Repetição
+{   
+    public static void main(String[] args) 
+    {
+        int x=13;
+
+        do
+        {
+            System.out.println("Você não tem permissão para entrar ainda");
+                x++;
+        }
+        while(x<10);
+        
+    }
+
+}
